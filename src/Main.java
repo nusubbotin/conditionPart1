de2a35d6d636aaa1d3f98d883160adcd00da5ffc
nusubbotin <nu.subbotin@ya.ru>
2022-09-11 11:36:37 +0300

    Реализация требований по заданию №2 (переписал с использованием конструкции else условного оператора)

--- src/Main.java
+++ src/Main.java
@@ -49,13 +49,28 @@ public class Main {
             System.out.println("вагон уже полностью забит");
         }
 
-        System.out.println("task4:");
+        System.out.println("task1New:"); //Доработка Задания №1
         int childAge = 15;
 
         if (childAge >= 18) {
             System.out.println("Поздравляем Вас с совершеннолетием!");
         } else {
             System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать");
+        }
+
+        System.out.println("task2New:"); //Доработка Задания №2
+        int teenagerAge = 25;
+
+        if (teenagerAge >= 24 ) {
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        } else {
+            if (teenagerAge >= 18) {
+                System.out.println("Человек уже закончил школу и может отправляться в университет");
+            } else {
+                if (teenagerAge >= 7) {
+                    System.out.println("Ребенок ходит в школу");
+                }
+            }
         }
     }
 }
