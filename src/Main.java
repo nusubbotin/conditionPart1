6d2e51f6ed2a05f70f11cd4534e77b3f349dc330
nusubbotin <nu.subbotin@ya.ru>
2022-09-11 10:38:42 +0300

    Реализация требований по заданию №2

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,19 @@ public class Main {
             System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать");
         }
 
+        System.out.println("task2:");
+        int personAge = 5;
+
+        if (personAge >= 7 && personAge < 18) {
+            System.out.println("Ребенок ходит в школу");
+        }
+
+        if (personAge >= 18 && personAge < 24) {
+            System.out.println("Человек уже закончил школу и может отправляться в университет");
+        }
+
+        if (personAge >= 24 ) {
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        }
     }
 }
