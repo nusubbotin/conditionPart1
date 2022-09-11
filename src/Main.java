85cc8d4bdd5e0307f2c844500c25a051961e7f6b
nusubbotin <nu.subbotin@ya.ru>
2022-09-11 10:32:17 +0300

    Реализация требований по заданию №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("task1:");
+        int age = 35;
+
+        if (age >= 18) {
+            System.out.println("Поздравляем Вас с совершеннолетием!");
+        }
+
+        if (age < 18) {
+            System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать");
+        }
+
     }
 }
